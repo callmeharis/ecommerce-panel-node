000a560cf1b9e844f66797baf7bf08b17894974b
izfaShoukatAli <izfashoukat800@gmail.com>
2024-07-07 02:28:55 -0700

    email confirmation

--- Controllers/orderController.js
+++ Controllers/orderController.js
@@ -1,18 +1,16 @@
 const cartModel = require('../Models/addtoCartModel');
 const Order = require('../Models/orderModel'); 
 const Shipping = require('../Models/shippingModel');
+const transporter=require('../middleware/emailTransporter')
 
 exports.createOrder = async (req, res) => {
     try {
         const { cartId, shippingId } = req.body;
 
-        // Fetch cart details based on cartId
         const cartDetails = await cartModel.findById(cartId).populate('products').exec();
 
-        // Fetch shipping details based on shippingId
         const shippingDetails = await Shipping.findById(shippingId).exec();
 
-        // Create new Order document
         const newOrder = new Order({
             cart: {
                 image: cartDetails.image,
@@ -32,10 +30,8 @@ exports.createOrder = async (req, res) => {
             }
         });
 
-        // Save the order to the database
         await newOrder.save();
 
-        // Respond with success message
         return res.status(201).json({
             success: true,
             message: "Order created successfully",
@@ -49,3 +45,58 @@ exports.createOrder = async (req, res) => {
         });
     }
 };
+
+
+exports.confirmationEmail = async (req, res) => {
+    try {
+        const { orderId, email } = req.body;
+
+        const order = await Order.findById(orderId)
+        if (!order) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+
+        const mailOptions = {
+            from: 'is170404123@gmail.com',
+            to: email, 
+            subject: 'Order Confirmation',
+            text: 'Your order has been placed successfully.',
+            html: `<p>Thank you for your order!</p>
+                   <p>Order Details:</p>
+                   <ul>
+                       <li>Order ID: ${order._id}</li>
+                       <li>Order Date: ${order.createdAt}</li>
+                       <li>Product Name:${order.cart.name},product Price${order.cart.price},Product Quantity${order.cart.quantity},</</</li>
+                       <li>Shipping Address: ${order.shipping.street}, ${order.shipping.city}, ${order.shipping.state}, ${order.shipping.country}</li>
+                   </ul>`
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        return res.status(200).json({ message: "Order confirmation email sent successfully" });
+    } catch (error) {
+        console.error("Error sending order confirmation email:", error);
+        return res.status(500).json({ message: "Failed to send order confirmation email", error: error.message });
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
